6d46631d8b410c42933e32f66a57bafd6452ac22
Lewis Johnson <LewisJohnsonDev@gmail.com>
2021-06-22 14:10:38 +0100

    Added ASP.NET Skeleton

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Scraper_ITDP.Models;
+
+namespace Scraper_ITDP.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		public IActionResult Scraper()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult ScrapeSearch(string url, string keywords)
+		{
+			return PartialView();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ Models/ResultsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scraper_ITDP.Models
+{
+	public class ResultsViewModel
+	{
+		public ResultsViewModel()
+		{
+			Results = new Dictionary<string, int>();
+		}
+
+		public ResultsViewModel(Dictionary<string, int> results)
+		{
+			Results = results ?? throw new ArgumentNullException(nameof(results));
+		}
+
+		public Dictionary<string, int> Results { get; set; }
+	}
+}
